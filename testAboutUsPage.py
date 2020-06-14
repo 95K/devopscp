ff026f6f930f8eed733d05df9898bb19d3aa27d0
Keyur <saxenakeyur@gmail.com>
2020-06-14 21:18:44 +0530

    commit

--- testAboutUsPage.py
+++ testAboutUsPage.py
@@ -2,7 +2,6 @@
 import unittest 
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys 
-
 # inherit TestCase Class and create a new test class 
 class DOCPVerifyAbout(unittest.TestCase): 
 
@@ -25,7 +24,7 @@ class DOCPVerifyAbout(unittest.TestCase):
 		button.click()
 
 		# the text we are searching for
-		text="This is <b>about</b> page. Lorem Ipsum Dipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+		text="This is <b>about</b> page."
 
 		# check if the text is in the page
 		self.assertTrue (text in driver.page_source) 
@@ -33,7 +32,6 @@ class DOCPVerifyAbout(unittest.TestCase):
 	# cleanup method called after every test performed 
 	def tearDown(self): 
 		self.driver.close() 
-
 # execute the script 
 if __name__ == "__main__": 
 	unittest.main()
